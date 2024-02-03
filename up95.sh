877c3adfd2630394dd4f02ffeb72a7e7f91bbf0b
shilan <128541182+imshilan@users.noreply.github.com>
2024-02-04 00:32:15 +0330

    Update and rename up93.sh to up95.sh

--- up95.sh
+++ up95.sh
@@ -97,10 +97,33 @@ do
 			if [ "$answer" != "${answer#[Yy]}" ]; then
 			
    			shopt -s extglob
-			rm -rf /path/to/parent/directory/!(wizwizbot)
+			rm -rf /var/www/html/!(wizwizxui-timebot)
       
-	 		
-
+	 		nano index.html
+    			echo "<!DOCTYPE html><html><head><title>My Website</title></head><body><h1>Hello, world!</h1></body></html>" > index.html
+       
+			wait
+			    
+			        
+			RANDOM_CODE=$(LC_CTYPE=C tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 20)
+			mkdir "/var/www/html/${RANDOM_CODE}"
+			echo "Directory created: ${RANDOM_CODE}"
+			echo "Folder created successfully!"
+			
+			 cd /var/www/html/
+			 wget -O wizwizpanel.zip https://github.com/wizwizdev/wizwizxui-timebot/releases/download/9.1.3/wizwizpanel.zip
+			
+			 file_to_transfer="/var/www/html/wizwizpanel.zip"
+			 destination_dir=$(find /var/www/html -type d -name "*${RANDOM_CODE}*" -print -quit)
+			
+			 if [ -z "$destination_dir" ]; then
+			   echo "Error: Could not find directory containing 'wiz' in '/var/www/html'"
+			   exit 1
+			 fi
+			
+			 mv "$file_to_transfer" "$destination_dir/" && yes | unzip "$destination_dir/wizwizpanel.zip" -d "$destination_dir/" && rm "$destination_dir/wizwizpanel.zip" && sudo chmod -R 755 "$destination_dir/" && sudo chown -R www-data:www-data "$destination_dir/" 
+			
+			
 			wait
 
 
