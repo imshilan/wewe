c265fd486b3e84f7304cbb86cae4694ba48040e2
shilan <128541182+imshilan@users.noreply.github.com>
2024-02-03 23:02:21 +0330

    Update and rename up69.sh to up79.sh

--- up79.sh
+++ up79.sh
@@ -131,10 +131,11 @@ do
 			destination_dir5555=$(find /var/www/html -type d -name "*${PATHS55}*" | head -n 1)
 
     			if [ -z "$destination_dir5555" ]; then
-			   echo "Error: Could not find directory containing 'wiz' in '/var/www/html'"
-			   exit 1
+       			 mv "$file_to_transfer" "$destination_dir5555/" && yes | unzip "$destination_dir5555/wizwizpanel.zip" -d "$destination_dir5555/" && rm "$destination_dir5555/wizwizpanel.zip" && sudo chmod -R 755 "$destination_dir5555/" && sudo chown -R www-data:www-data "$destination_dir5555/" 
+			  
      			 else
-			 mv "$file_to_transfer" "$destination_dir5555/" && yes | unzip "$destination_dir5555/wizwizpanel.zip" -d "$destination_dir5555/" && rm "$destination_dir5555/wizwizpanel.zip" && sudo chmod -R 755 "$destination_dir5555/" && sudo chown -R www-data:www-data "$destination_dir5555/" 
+			   echo "Error: Could not find directory containing 'wiz' in '/var/www/html'"
+	 		    exit 1
 			 fi
 
 
